c71a40d1fad6655ad60a18c4def43bf1b1201ed3
hanguixian <1129982332@qq.com>
2019-10-17 00:08:31 +0800

    spring activeMQ 生产者

---
+++ spring-activemq/src/main/java/com/hgx/spring/activemq/produce/ActiveMQProduce.java
@@ -0,0 +1,25 @@
+package com.hgx.spring.activemq.produce;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.jms.TextMessage;
+
+@Component
+public class ActiveMQProduce {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ActiveMQProduce activeMQProduce = (ActiveMQProduce) applicationContext.getBean("activeMQProduce");
+        activeMQProduce.jmsTemplate.send(session -> {
+            return session.createTextMessage("-------spring activeMQ message--------");
+        });
+        System.out.println("--------spring message send success-----------");
+    }
+}
